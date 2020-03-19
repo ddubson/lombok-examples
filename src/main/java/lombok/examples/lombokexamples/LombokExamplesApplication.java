c90883ace11d70349fb24317ff1e9891a029112a
Dmitriy Dubson <ddubson@pivotal.io>
2020-03-19 15:03:44 -0400

    Initial Gradle project import

---
+++ src/main/java/lombok/examples/lombokexamples/LombokExamplesApplication.java
@@ -0,0 +1,13 @@
+package lombok.examples.lombokexamples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LombokExamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LombokExamplesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lombok/examples/lombokexamples/LombokExamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package lombok.examples.lombokexamples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LombokExamplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

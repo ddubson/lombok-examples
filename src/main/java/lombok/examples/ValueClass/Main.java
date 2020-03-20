fe8b3a4cc23db82c30ad477183ba07d72b90a823
Dmitriy Dubson <ddubson@pivotal.io>
2020-03-20 11:26:50 -0400

    Add helpers

--- src/main/java/lombok/examples/ValueClass/Main.java
+++ src/main/java/lombok/examples/ValueClass/Main.java
@@ -1,11 +1,14 @@
 package lombok.examples.ValueClass;
 
+import static lombok.examples.Helpers.withLombok;
+import static lombok.examples.Helpers.withoutLombok;
+
 public class Main {
 	public static void main(String[] args) {
 		OldUserProfile oldUserProfile = new OldUserProfile("James", "Doe", "111-111-1111", "222-222-2222");
-		System.out.println("Without Lombok: " + oldUserProfile.toString());
+		withoutLombok(oldUserProfile.toString());
 
 		NewUserProfile newUserProfile = new NewUserProfile("James", "Doe", "111-111-1111", "222-222-2222");
-		System.out.println("With Lombok: " + newUserProfile.toString());
+		withLombok(newUserProfile.toString());
 	}
 }
